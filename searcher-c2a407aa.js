4dd396dc441bc869de447195d8e83b4ec7419930
LDeakin <LDeakin@users.noreply.github.com>
2026-02-01 15:27:42 +0000

    deploy: e4ce096ea8d7f2f837ad1b1de0a43faeb930d126

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ce85899b.js',
+                path_to_root + 'searchindex-d44daf2e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
